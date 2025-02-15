3d8c21b18c7e310c02286c443b79281bc1644135
sanalkumars <sanalskumar126@gmail.com>
2025-02-15 20:26:23 +0530

    basic setup done with shadcnui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      hello
-      </main>
-    
-    </div>
+    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto">
+      <h1 className="text-5xl font-bold">Invoice Creator</h1>
+      <p>
+        <Button asChild>
+          <Link href="/dashboard">Sign In</Link>
+        </Button>
+      </p>
+    </main>
   );
 }
